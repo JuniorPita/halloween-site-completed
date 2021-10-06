fc37e7df978a66b29d362bed646d3e3211923c61
Evgeny <sobolenok119@mail.ru>
2021-10-06 16:04:54 +0300

    Added new sections and their styles

---
+++ HALLOWEEN-SITE (Completed)/js/script.js
@@ -0,0 +1,36 @@
+// Show-Hide menu
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+// Show menu
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+}
+
+// Hide menu
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
+
+// Remove menu mobile
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Home swiper
+let homeSwiper = new Swiper('.home-swiper', {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
