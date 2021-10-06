e39fc80a43040463b262ecfdba6a85bf29416b7f
Evgeny <sobolenok119@mail.ru>
2021-10-06 17:07:29 +0300

    Added new sections and their styles

--- HALLOWEEN-SITE (Completed)/js/script.js
+++ HALLOWEEN-SITE (Completed)/js/script.js
@@ -31,6 +31,19 @@ let homeSwiper = new Swiper('.home-swiper', {
     loop: 'true',
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
-    },
+        clickable: true
+    }
 });
+
+// Change background header
+function scrollHeader() {
+    const header = document.getElementById('header');
+
+    if (this.scrollY >= 50) {
+        header.classList.add('scroll-header');
+    }
+    else {
+        header.classList.remove('scroll-header');
+    }
+}
+window.addEventListener('scroll', scrollHeader);
