e60cd3e05e8d333a7173932ec5d0f6dd2bb4a6bc
Evgeny <sobolenok119@mail.ru>
2021-10-07 16:00:10 +0300

    Added new sections, their styles and scripts with the media queries

--- #src/js/script.js
+++ #src/js/script.js
@@ -47,3 +47,59 @@ function scrollHeader() {
     }
 }
 window.addEventListener('scroll', scrollHeader);
+
+// New Arrivals Swiper
+let newSwiper = new Swiper('.new-swiper', {
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    loop: 'true'
+});
+
+// Scroll sections active link
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        }
+        else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
+}
+window.addEventListener('scroll', scrollActive);
+
+// Show scroll up
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+
+    if (this.scrollY >= 460) {
+        scrollUp.classList.add('show-scroll');
+    }
+    else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll', scrollUp);
+
+// Scroll reveal animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: false
+});
+
+sr.reveal('.home-swiper, .new-swiper, .newsletter__container');
+sr.reveal('.category__data, .trick__content, .footer__content', {interval: 100});
+sr.reveal('.about__data, .discount__img', {origin: 'left'});
+sr.reveal('.about__img, .discount__data', {origin: 'right'});
